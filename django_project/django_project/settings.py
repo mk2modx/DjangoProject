b41ca8a591cc9427aa6ad26c9890c37c15eec850
Agbayani <Mark.Agbayani@centurylink.com>
2020-03-10 15:02:24 -0600

    working with password reset

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -79,6 +79,17 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    #  'default': {
+    #     'ENGINE': 'djongo',
+    #     'NAME':'howlegitdb',
+    #     'CLIENT': {
+    #             'host': 'mongodb://10.193.143.162:27017/?compressors=disabled&gssapiServiceName=mongodb',
+    #             'port': 27017,
+    #             'authSource': 'howlegitdb',
+    #         }
+ 
+    # }
+
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
